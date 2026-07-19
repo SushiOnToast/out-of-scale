e1dccba287481e587541ecfbad918e1c50367934
SushiOnToast <sushbomm@gmail.com>
2026-07-19 09:58:45 +0530

    add menu cursors

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,10 @@
 extends Area2D
-var holding_texture = preload("res://graphics/characters/player/player_holding.png")
-var idle_texture = preload("res://graphics/characters/player/player_idle.png")
+
+@export var click_texture = preload("res://graphics/characters/player/player_holding.png")
+@export var idle_texture = preload("res://graphics/characters/player/player_idle.png")
+
+@export var menu = false
+
 var minigame_texture = preload("res://graphics/characters/player/player_minigame.png")
 var minigame_click_texture = preload("res://graphics/characters/player/player_minigame_click.png")
 var hover_item : CharacterBody2D
@@ -13,8 +17,10 @@ func get_input():
 	if Global.minigame_mode:
 		$Sprite2D.texture = minigame_click_texture if Input.is_action_pressed("use") else minigame_texture
 		scale = Vector2(0.5, 0.5)
+	elif menu:
+		$Sprite2D.texture = click_texture if Input.is_action_pressed("use") else idle_texture
 	else:
-		$Sprite2D.texture = holding_texture if Input.is_action_pressed("grab") else idle_texture
+		$Sprite2D.texture = click_texture if Input.is_action_pressed("grab") else idle_texture
 		scale = Vector2.ONE
 
 		if Input.is_action_just_released("grab"):
@@ -37,7 +43,7 @@ func _process(_delta: float) -> void:
 	position = mouse_pos
 
 	if current_item:
-		$Sprite2D.texture = holding_texture
+		$Sprite2D.texture = click_texture
 		current_item.position = mouse_pos - Vector2(7.5, 7.5)
 
 func _on_body_entered(body: CharacterBody2D) -> void:
